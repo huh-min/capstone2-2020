bcb412b786f71fd11bc59a6804b1ada10135d30b
jeongyebin <gsj4243@kookmin.ac.kr>
2020-11-18 21:51:28 +0900

    feat:cody review modify button

--- web/src/components/cards/ClothesSetReviewCard.vue
+++ web/src/components/cards/ClothesSetReviewCard.vue
@@ -43,9 +43,14 @@
         <b-img src="@/assets/rain.png" width="30px" />
         {{ round(review.precipitation, 1) }} mm
       </p>
-      <b-col>
-        <b-button @click="deleteCodyReview">삭제하기</b-button>
-      </b-col>
+      <b-row>
+        <b-col>
+          <b-button size="sm" @click="deleteCodyReview">삭제</b-button>
+        </b-col>
+        <b-col>
+          <b-button size="sm" @click="modifyCodyReview">수정</b-button>
+        </b-col>
+      </b-row>
     </template>
   </b-overlay>
 </template>
