7abc220cb837936c3a916ab40f5e5a4e50822634
jeongyebin <gsj4243@kookmin.ac.kr>
2020-11-29 02:49:42 +0900

    feat: get review info

--- web/src/components/ReviewModifyComponent.vue
+++ web/src/components/ReviewModifyComponent.vue
@@ -28,7 +28,6 @@ import consts from '@/consts.js'
 import axios from 'axios'
 
 export default {
-  name: 'reviewmodifycomponent',
   data: function () {
     return {
       showAlert: false,
@@ -37,7 +36,47 @@ export default {
   },
   props: [
     'review'
-  ]
+  ],
+  created: function () {
+    var vm = this
+    if (!localStorage.getItem('token')) {
+      this.$router.push({
+        name: 'Bridge',
+        params: {
+          errorMessage: '로그인이 필요한 서비스입니다.',
+          destination: 'login',
+          delay: 3,
+          variant: 'danger'
+        }
+      })
+    } else {
+      if (vm.review.id === undefined) {
+        this.$router.push({
+          name: 'Bridge',
+          params: {
+            errorMessage: '해당 리뷰가 없습니다.',
+            destination: 'Cody',
+            delay: 3,
+            variant: 'danger'
+          }
+        })
+      } else {
+        var token = window.localStorage.getItem('token')
+        var config = {
+          headers: { Authorization: `Bearer ${token}` }
+        }
+        var url = consts.SERVER_BASE_URL + '/clothes-set-reviews/'
+        url += this.review.id + '/'
+        axios.get(url, config)
+          .then(response => {
+            console.log(response)
+          }).catch((ex) => {
+            this.alertMessage = '코디 리뷰를 불러오지 못하였습니다. 오류가 계속 될 경우, 관리자에게 연락해주세요.'
+            this.showAlert = true
+          })
+      }
+    }
+  }
 }
 </script>
 
